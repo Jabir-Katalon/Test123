7613adca46c92a1953b42a2137d472337774255a
mohammedjabir <mohammedjabir@localhost>
2024-07-18 18:06:42 +0530

    Test123

--- Scripts/Test1/Script1720631651483.groovy
+++ Scripts/Test1/Script1720631651483.groovy
@@ -29,15 +29,17 @@ WebUI.closeBrowser()
 
 ChromeOptions options = new ChromeOptions()
 
-//def driverPath = System.getProperty("user.dir") + "/Include/drivers/chromedriver_mac64/chromedriver";
 
 
-//System.setProperty("webdriver.chrome.driver", driverPath);
+def driverPath = System.getProperty("user.dir") + "/Include/drivers/chromedriver_mac64/chromedriver";
+
+
+System.setProperty("webdriver.chrome.driver", driverPath);
 
 // This option disables the Chrome security settings that prevent local file access
 
 options.addArguments('allow-file-access-from-files')
 
- 
+Thread.sleep(15000)
 
 DriverFactory.changeWebDriver(new ChromeDriver(options))
