53fd19c921793a2c1bcc554ff63b5db96343185d
mohammedjabir <mohammedjabir@fe80:0:0:0:a819:59ff:feb5:411c%13>
2024-12-26 16:43:38 +0530

    Test

---
+++ Test Suites/Cucumber Tag Execution/Cucumber Tag Execution.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Cucumber Tag Execution</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6a690e6e-ca0a-4960-b997-a75bf7b4e09d</testSuiteGuid>
+</TestSuiteEntity>
